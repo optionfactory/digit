a5c27775f5ddf5bff1503695737092e0ca7540ad
Davide Salvador <davide@optionfactory.net>
2016-04-12 14:22:37 +0200

    all resources are now local

--- js/main.js
+++ js/main.js
@@ -45,7 +45,8 @@ if (!Array.prototype.indexOf) {
 
 require.config({
     paths: {
-        'd3': 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.12/d3.min'
+ //       'd3': 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.12/d3.min'
+        'd3': 'js/d3.min'
     },
     shim: {
         'd3': {
