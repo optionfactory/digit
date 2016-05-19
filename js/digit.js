8b5ffe5a49c8cc8f67a258ad9fe56513c475631f
Davide Salvador <davide@optionfactory.net>
2016-05-19 16:00:41 +0200

    :enh: better websocket.onclose handling

--- js/digit.js
+++ js/digit.js
@@ -2,17 +2,18 @@ function connect(name) {
     var socket = new WebSocket("ws://localhost:9000/ws?name=" + name);
     socket.onopen = function() {
         var repoViewer = new RepoViewer();
-        repoViewer.render(d3.select("#"+name));
+        repoViewer.render(d3.select("#" + name));
         var firstUpdate = true;
         socket.onmessage = function(evt) {
             repoViewer.update(JSON.parse(evt.data), firstUpdate);
-            firstUpdate=false;
+            firstUpdate = false;
         };
     };
     socket.onerror = function() {
         socket.close();
     }
-    socket.onclose = function() {
+    socket.onclose = function(evt) {
+        console.log("ws connection closed with code '" + evt.code + "' and reason '" + evt.reason + "' (clean='" + evt.wasClean + "')");
         setTimeout(connect.bind(null, path), 1000);
     }
 };
