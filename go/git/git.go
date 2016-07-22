7fb604f3a473b599773c816fbdf03ec6df9fcb93
Francesco Degrassi <francesco@optionfactory.net>
2016-07-22 17:30:23 +0200

    :new: full data fetch and push.

---
+++ go/git/git.go
@@ -0,0 +1,330 @@
+package git
+
+import (
+	"bufio"
+	"errors"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+type Commit struct {
+	Id             string   `json:"id"`
+	Unreachable    bool     `json:"unreachable"`
+	Parents        []string `json:"parents"`
+	AuthorName     string   `json:"author_name"`
+	AuthorEmail    string   `json:"author_email"`
+	AuthorDate     string   `json:"author_date"`
+	CommitterName  string   `json:"committer_name"`
+	CommitterEmail string   `json:"committer_email"`
+	CommitterDate  string   `json:"committer_date"`
+	Message        string   `json:"message"`
+}
+
+type Repo struct {
+	Path           string       `json:"path"`
+	Name           string       `json:"name"`
+	Commits        []Commit     `json:"commits"`
+	Tags           []Ref        `json:"tags"`
+	Stash          []Ref        `json:"stash"`
+	Branches       []Ref        `json:"branches"`
+	RemoteBranches []Ref        `json:"remoteBranches"`
+	Head           HeadRef      `json:"head"`
+	Status         []FileStatus `json:"status"`
+}
+
+type FileStatus struct {
+	Filename             string `json:"filename"`
+	WorkingCopyToStaging string `json:"workingCopyToStaging"`
+	StagingToCommit      string `json:"stagingToCommit"`
+}
+
+func New(name string, repositoryDir string) *Repo {
+	return &Repo{
+		Name: name,
+		Path: repositoryDir,
+	}
+}
+
+func (self *Repo) Update() *Repo {
+	self.Branches = self.localBranches()
+	self.RemoteBranches = self.remoteBranches()
+	self.Stash = self.stashContent()
+	self.Tags = self.tags()
+	self.Head = self.headCommit()
+	self.Commits = self.reachables()
+	self.Commits = append(self.Commits, self.unreachables()...)
+	self.Status = self.status()
+	return self
+}
+
+type filter func(line string) bool
+
+func always(line string) bool {
+	return true
+}
+
+type Ref struct {
+	Id   string `json:"id"`
+	Hash string `json:"commitId"`
+}
+
+func (self *Repo) readReferences(opts []string, filter filter) ([]Ref, error) {
+	args := []string{"-C", self.Path, "show-ref"}
+	args = append(args, opts...)
+	cmd := exec.Command("git", args...)
+	stderr, _ := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	result := make([]Ref, 0)
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), " \t")
+		if line == "" {
+			continue
+		}
+		if filter(line) {
+			ref, err := parseRef(scanner.Text())
+			if err == nil {
+				result = append(result, ref)
+			}
+		}
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Println(err)
+	}
+
+	sscanner := bufio.NewScanner(stderr)
+	for sscanner.Scan() {
+		log.Println(sscanner.Text())
+	}
+	return result, nil
+}
+
+func (self *Repo) readHEADRef() (string, error) {
+	args := []string{"-C", self.Path, "symbolic-ref", "--short", "HEAD"}
+	cmd := exec.Command("git", args...)
+	stderr, _ := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	if err := cmd.Start(); err != nil {
+		log.Println(err)
+		return "", err
+	}
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), " \t")
+		if line == "" {
+			continue
+		}
+		return scanner.Text(), nil
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Println(err)
+	}
+
+	sscanner := bufio.NewScanner(stderr)
+	for sscanner.Scan() {
+		log.Println(sscanner.Text())
+	}
+	return "", nil
+}
+
+func (self *Repo) readLines(opts []string, filter filter) ([]string, error) {
+	args := []string{"-C", self.Path}
+	args = append(args, opts...)
+	cmd := exec.Command("git", args...)
+	stderr, _ := cmd.StderrPipe()
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	result := make([]string, 0)
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), " \t")
+		if line == "" {
+			continue
+		}
+		if filter(line) {
+			result = append(result, scanner.Text())
+		}
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Println(err)
+	}
+
+	sscanner := bufio.NewScanner(stderr)
+	for sscanner.Scan() {
+		log.Println(sscanner.Text())
+	}
+	return result, nil
+}
+
+func parseRef(text string) (Ref, error) {
+	refParts := strings.SplitN(text, " ", 2)
+	hash := refParts[0]
+	idParts := strings.SplitN(refParts[1], "/", 3)
+	id := idParts[len(idParts)-1]
+	if strings.HasSuffix(id, "^{}") {
+		return Ref{}, errors.New("^{}")
+	}
+	return Ref{
+		Id:   id,
+		Hash: hash,
+	}, nil
+}
+
+func (self *Repo) tags() []Ref {
+	res, err := self.readReferences([]string{"--tags", "-d"}, always)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func (self *Repo) localBranches() []Ref {
+	res, err := self.readReferences([]string{"--heads"}, always)
+	if err != nil {
+		panic(err)
+	}
+	return res
+
+}
+
+func (self *Repo) remoteBranches() []Ref {
+	res, err := self.readReferences([]string{}, func(line string) bool {
+		return strings.Contains(line, "refs/remotes/")
+	})
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func (self *Repo) stashContent() []Ref {
+	res, err := self.readReferences([]string{}, func(line string) bool {
+		return strings.Contains(line, "refs/stash/")
+	})
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+type HeadRef struct {
+	BranchId string `json:"branchId"`
+	//CommitId string `json:"commitId"`
+}
+
+func (self *Repo) headCommit() HeadRef {
+	refName, err := self.readHEADRef()
+	if err != nil {
+		panic(err)
+	}
+	if len(refName) > 0 {
+		return HeadRef{
+			BranchId: refName,
+		}
+	}
+	refs, err2 := self.readReferences([]string{"--head"}, func(line string) bool {
+		return strings.Contains(line, " HEAD")
+	})
+	if err2 != nil {
+		panic(err)
+	}
+	_ = refs
+	return HeadRef{
+	//		CommitId: refs[0].Hash,
+	}
+}
+
+func (self *Repo) reachables() []Commit {
+	lines, err := self.readLines([]string{"log", "--pretty=%H|%P|%an|%ae|%ad|%cn|%ce|%cd|%s", "--reverse", "--all"}, always)
+	if err != nil {
+		panic(err)
+	}
+	commits := make([]Commit, 0, len(lines))
+	for _, line := range lines {
+		commits = append(commits, parseCommit(line, false))
+	}
+	return commits
+}
+
+func (self *Repo) unreachables() []Commit {
+	idLines, err := self.readLines([]string{"fsck", "--unreachable", "--no-reflogs", "--no-progress"}, func(line string) bool {
+		return strings.Contains(line, "commit")
+	})
+	if err != nil {
+		panic(err)
+	}
+	unreacheableIds := make([]string, 0, len(idLines))
+	for _, line := range idLines {
+		unreacheableIds = append(unreacheableIds, strings.SplitN(line, " ", 3)[2])
+	}
+
+	revListArgs := []string{"rev-list", "--no-walk", "--pretty=%H|%P|%an|%ae|%ad|%cn|%ce|%cd|%s"}
+	revListArgs = append(revListArgs, unreacheableIds...)
+	lines, err := self.readLines(revListArgs, func(line string) bool {
+		return !strings.HasPrefix(line, "commit")
+	})
+	commits := make([]Commit, 0, len(lines))
+	for _, line := range lines {
+		commits = append(commits, parseCommit(line, true))
+	}
+	return commits
+}
+
+func parseCommit(line string, unreachable bool) Commit {
+	parts := strings.SplitN(line, "|", 9)
+	parentIds := make([]string, 0)
+	if len(parts[1]) > 0 {
+		parentIds = strings.Split(parts[1], " ")
+	}
+	return Commit{
+		Id:             parts[0],
+		Unreachable:    unreachable,
+		Parents:        parentIds,
+		AuthorName:     parts[2],
+		AuthorEmail:    parts[3],
+		AuthorDate:     parts[4],
+		CommitterName:  parts[5],
+		CommitterEmail: parts[6],
+		CommitterDate:  parts[7],
+		Message:        parts[8],
+	}
+}
+
+func (self *Repo) status() []FileStatus {
+	lines, err := self.readLines([]string{"status", "--porcelain"}, always)
+	if err != nil {
+		panic(err)
+	}
+	statuses := make([]FileStatus, 0, len(lines))
+	for _, line := range lines {
+		stagingToCommit := line[0:1]
+		workingCopyToStaging := line[1:2]
+		filename := line[3:]
+		statuses = append(statuses, FileStatus{
+			Filename:             filename,
+			WorkingCopyToStaging: workingCopyToStaging,
+			StagingToCommit:      stagingToCommit,
+		})
+	}
+	return statuses
+}
--- go/main.go
+++ go/main.go
@@ -5,48 +5,38 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/howeyc/fsnotify"
+	"github.com/optionfactory/digit/git"
 	// "golang.org/x/net/websocket"
 	"github.com/tjgq/broadcast"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 var version string
 
 var templates = template.Must(template.ParseFiles("index.template.go"))
 
-type Repo struct {
-	Name string
-	Path string
-}
-
-type Update struct {
-	RepoName string
-}
-
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
-func echoHandler(updateListener *broadcast.Listener, w http.ResponseWriter, r *http.Request) {
-	repoName := r.FormValue("name")
-	log.Println("listening for", repoName)
+func echoHandler(updateListener *broadcast.Listener, solicitations chan bool, repoName string, w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
+	solicitations <- true
 	for updateEvent := range updateListener.Ch {
-		update, ok := updateEvent.(*Update)
+		update, ok := updateEvent.(git.Repo)
 		if !ok {
 			continue
 		}
-		log.Println("cast gave", ok)
-		if repoName != update.RepoName {
+		if repoName != update.Name {
 			continue
 		}
 		err = conn.WriteJSON(update)
@@ -57,19 +47,22 @@ func echoHandler(updateListener *broadcast.Listener, w http.ResponseWriter, r *h
 	}
 }
 
+func parseRepo(name string, path string) *git.Repo {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	return git.New(name, absPath)
+}
+
 func main() {
 	fmt.Fprintf(os.Stderr, "starting version %v\n", version)
 
 	// TODO: help and options docs
-	target_repos := []Repo{{
-		Name: "local",
-		Path: os.Args[1],
-	}}
+	target_repos := []*git.Repo{parseRepo("local", os.Args[1])}
 	if len(os.Args) > 2 {
-		target_repos = append(target_repos, Repo{
-			Name: "remote",
-			Path: os.Args[2],
-		})
+		target_repos = append(target_repos, parseRepo("remote", os.Args[2]))
 	}
 	var repo_names = []string{}
 	for _, r := range target_repos {
@@ -83,26 +76,32 @@ func main() {
 		}
 	})
 	var updates broadcast.Broadcaster
+
+	solicitationsMap := make(map[string]chan bool)
+
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		repoName := r.FormValue("name")
+		log.Println("listening for", repoName)
 		updateListener := updates.Listen()
 		defer updateListener.Close()
-		echoHandler(updateListener, w, r)
+		echoHandler(updateListener, solicitationsMap[repoName], repoName, w, r)
 	})
 	http.Handle("/", http.FileServer(http.Dir(".")))
 
 	for _, r := range target_repos {
+		solicitationsMap[r.Name] = make(chan bool)
 		watcher, err := fsnotify.NewWatcher()
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		go func() {
 			for {
 				select {
+				case _ = <-solicitationsMap[r.Name]:
+					r.Update()
+					updates.Send(*r)
 				case event := <-watcher.Event:
-					updates.Send(&Update{
-						RepoName: r.Name,
-					})
+					solicitationsMap[r.Name] <- true
 					log.Println("event:", event)
 				case err := <-watcher.Error:
 					// TODO
